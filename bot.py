c70ee617ca7908c25813b1e22bcfc865f3312ea4
litotaep <gazamat666@mail.ru>
2025-12-15 17:23:51 +0300

    feat: add real-time plant growth with time module fix: correct object serialization for game saving

--- bot.py
+++ bot.py
@@ -9,11 +9,13 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(script_dir, "saved_data.json")
 
 class Plant:
-    def __init__(self, name, growth_stages):
+    def __init__(self, name, growth_stages, growth_time):
         self.name = name
+        self.growth_time= growth_time
         self.growth_stages = growth_stages
         self.current_stage = 0
         self.is_ripe = False
+        self.planted_time = 0
     @property
     def symbol(self):
         return self.growth_stages[self.current_stage]
@@ -34,9 +36,9 @@ class Plant:
     
 
 plants_species = {
-    "tomatoes": Plant("tomatoes", [".","🪴","🍅"]),
-    "potatoes": Plant("potatoes", ["*","🌿","🥔"]),
-    "cucumbers": Plant("cucumbers",["O","🌱","🥒"])
+    "tomatoes": Plant("tomatoes", [".","🪴","🍅"], 5),
+    "potatoes": Plant("potatoes", ["*","🌿","🥔"], 10),
+    "cucumbers": Plant("cucumbers",["O","🌱","🥒"], 15)
 }
 
 
@@ -52,11 +54,24 @@ def create_new_field(width_and_height):
 
 
 def save_data(*, field, inventory, file_path):  
+    save_field = []
+    for row in field:
+        save_row = []
+        for cell in row:
+            if cell is None:
+                save_row.append(None)
+            else:
+                save_row.append({
+                    "name": cell.name,
+                    "stage": cell.current_stage,
+                    "is_ripe": cell.is_ripe
+                })
+        save_field.append(save_row)
+    data = {
+        "field": save_field,
+        "inventory": inventory
+    }
     try:
-        data = {
-            "field" : field,
-            "inventory": inventory
-        } 
         with open(file_path, 'w', encoding='utf-8') as f:
             json.dump(data, f, indent=4, ensure_ascii=False)
             print("Successfully saved!")
@@ -155,6 +170,25 @@ def check_saved_inventory(*, file_path):
     except Exception as e:
         print(f"Error: {e}")
 
+def dict_to_object(*, field, plants_species):
+    new_field = []
+    for row in field:
+        new_row = []
+        for cell in row:
+            if cell is None:
+                new_row.append(None)
+            else:
+                plant_template = plants_species[cell["name"]]
+                plant = Plant(
+                    name=plant_template.name, 
+                    growth_stages=plant_template.growth_stages,
+                    growth_time=plant_template.growth_time)
+                plant.current_stage = cell["stage"]
+                plant.is_ripe = cell["is_ripe"]
+                new_row.append(plant)
+        new_field.append(new_row)
+    return new_field    
+
 def check_saved_field(*, file_path):
     try:
         with open(file_path, 'r', encoding="utf-8") as f:
@@ -162,7 +196,8 @@ def check_saved_field(*, file_path):
         for key in data: 
             if key== "field":
                 field = data.get("field")
-                return field
+                object_field = dict_to_object(field=field, plants_species=plants_species)
+                return object_field
         return None
     except FileNotFoundError: 
         return None
@@ -172,16 +207,41 @@ def check_saved_field(*, file_path):
         print(f"Error: {e}") 
 
 def planting(*,plant,x,y, field, inventory):
-    field[x][y] = plant
-    inventory_change(inventory=inventory, action="planting_plant", plant=plant)
+    if field[x][y] is None:
+        field[x][y] = plant
+        plant.planted_time = time.time()
+        inventory_change(inventory=inventory, action="planting_plant", plant=plant)
+    else:
+        print("There is another plant in this cell")
 
+def check_growth(*, field):
+    new_field = []
+    for row in field:
+        new_row = []
+        for cell in row:
+            if cell is None:
+                new_row.append(None)
+            else:
+                if cell.growth_time*2 > (time.time() - cell.planted_time) >= cell.growth_time:
+                    cell.grow()
+                    new_row.append(cell)
+                elif cell.growth_time*2 <= (time.time() - cell.planted_time):
+                    cell.grow()
+                    cell.grow()
+                    new_row.append(cell)
+                else:
+                    new_row.append(cell)
+        new_field.append(new_row)
+    return new_field  
+
+        
         
 def harvest(*,x,y, field, inventory):
     if field[x][y] != None:
         plant = field[x][y]
         collected_plant = plant.harvest()
         if collected_plant is None:
-            print(f"Sorry! The {collected_plant} is not ready yet!")
+            print(f"Sorry! The {plant.name} is not ready yet!")
             return
         field[x][y] = None
         print(f"{plant.name} successfully collected!")
@@ -193,7 +253,7 @@ def harvest(*,x,y, field, inventory):
 def game_loop(field, inventory):
     show_field(field)
     while True:
-        answer = input("\n1. Inventory\n2. Plant\n3. Show field\n4. Save\n5. Harvest\n6. Quit\nChoose (1-6): ")
+        answer = input("\n1. Inventory\n2. Plant\n3. Show field\n4. Save\n5. Check field\n6. Harvest\n7. Quit\nChoose (1-6): ")
         if answer == "1":
             show_inventory(inventory)
         elif answer == "2":
@@ -209,9 +269,12 @@ def game_loop(field, inventory):
         elif answer == "4":
             save_data(field=field,inventory=inventory ,file_path=file_path)
         elif answer == "5":
+            field = check_growth(field=field)
+            show_field(field=field)
+        elif answer == "6":
             x, y = get_coordinates(len(field))
             harvest(x=x, y=y, field=field, inventory=inventory)
-        elif answer == "6":
+        elif answer == "7":
             sys.exit()
         
 
@@ -242,15 +305,3 @@ def main_game():
 main_game()    
 
 
-
-# inventory = {
-#             "tomatoes": 0,
-#             "potatoes": 3,
-#             "cucumbers": 3
-#         }
-# field = create_new_field(3)
-# x, y = get_coordinates(len(field))
-# plant = share_plant(plants_species, inventory) 
-# if plant is not None:   
-#     planting(seed=plant.name, plant=plant, x=x, y=y, field=field, inventory=inventory)
-# show_field(field)
